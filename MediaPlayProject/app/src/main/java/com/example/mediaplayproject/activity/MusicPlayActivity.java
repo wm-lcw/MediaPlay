e6ef5ea60376dd9b2b0acd5a94cc7bb5aeb73cd3
wmbilibili <2609228707@qq.com>
2023-02-01 18:52:38 +0800

    V1.0.2 完善音乐列表UI浮窗、音量同步 [author]        wm [description] 新加功能：点击列表浮窗外部可隐藏浮窗；音量条与设备music音量同步

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/activity/MusicPlayActivity.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/activity/MusicPlayActivity.java
@@ -1,23 +1,35 @@
 package com.example.mediaplayproject.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.SeekBar;
 
 import com.example.mediaplayproject.R;
+import com.example.mediaplayproject.adapter.MusicAdapter;
 import com.example.mediaplayproject.bean.MediaFileBean;
 import com.example.mediaplayproject.utils.DebugLog;
 import com.example.mediaplayproject.utils.SearchFiles;
@@ -34,12 +46,19 @@ public class MusicPlayActivity extends AppCompatActivity {
 
     private ImageView ivMediaLoop, ivMediaPre, ivMediaPlay, ivMediaNext, ivMediaList, ivCloseListView;
     private SeekBar sbVolume, sbProgress;
-    private boolean playState = false;
+    private ListView mMusicListView;
+    private boolean playState = false, isShowList = false, mRegistered = false;
     private Context mContext;
     private List<MediaFileBean> musicInfo = new ArrayList<>();
     private LinearLayout mFloatLayout;
     private WindowManager.LayoutParams wmParams;
     private WindowManager mWindowManager;
+    private AudioManager mAudioManager;
+    private MusicBroadcastReceiver mMusicBroadcastReceiver;
+    private static final String EXTRA_VOLUME_STREAM_TYPE = "android.media.EXTRA_VOLUME_STREAM_TYPE";
+    private final String VOLUME_CHANGE_ACTION = "android.media.VOLUME_CHANGED_ACTION";
+    private final String VOLUME_MUTE = "android.media.STREAM_MUTE_CHANGED_ACTION";
+    private final static int HANDLER_MESSAGE_REFRESH_VOLUME = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,19 +66,22 @@ public class MusicPlayActivity extends AppCompatActivity {
         setContentView(R.layout.activity_music_play);
         mContext = this;
         updateMusicFiles();
+        registerReceiver();
         initData();
+
     }
 
     private void updateMusicFiles() {
         SearchFiles mSearcherFiles = SearchFiles.getInstance(mContext);
         musicInfo = mSearcherFiles.getMusicInfo();
-        if (musicInfo.size() > 0) {
-            Iterator<MediaFileBean> iterator = musicInfo.iterator();
-            while (iterator.hasNext()) {
-                MediaFileBean mediaFileBean = iterator.next();
-                DebugLog.debug(mediaFileBean.getTitle() + " -- " + mediaFileBean.getData());
-            }
-        }
+        //打印输出音乐列表
+//        if (musicInfo.size() > 0) {
+//            Iterator<MediaFileBean> iterator = musicInfo.iterator();
+//            while (iterator.hasNext()) {
+//                MediaFileBean mediaFileBean = iterator.next();
+//                DebugLog.debug(mediaFileBean.getTitle() + " -- " + mediaFileBean.getData());
+//            }
+//        }
     }
 
     @Override
@@ -67,6 +89,8 @@ public class MusicPlayActivity extends AppCompatActivity {
         super.onResume();
         ivMediaPlay.setImageResource(playState ? R.mipmap.media_pause : R.mipmap.media_play);
     }
+
+
 
     private void initData() {
         ivMediaLoop = findViewById(R.id.bt_loop);
@@ -86,6 +110,13 @@ public class MusicPlayActivity extends AppCompatActivity {
         sbVolume.setOnSeekBarChangeListener(mSeekBarListener);
         sbProgress.setOnSeekBarChangeListener(mSeekBarListener);
 
+        mAudioManager = (AudioManager) mContext.getSystemService(Context.AUDIO_SERVICE);
+        int currentVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+        DebugLog.debug("getCurrentVolume " + currentVolume);
+        if (currentVolume >= 0 && currentVolume <= 150){
+            int afterVolume = (int) (currentVolume/1.5);
+            sbVolume.setProgress(afterVolume);
+        }
 
     }
 
@@ -110,6 +141,7 @@ public class MusicPlayActivity extends AppCompatActivity {
                 DebugLog.debug("onclick ivMediaNext");
             } else if (view == ivMediaList) {
                 DebugLog.debug("onclick ivMediaList");
+                isShowList = true;
                 createFloatView();
             }
         }
@@ -120,6 +152,11 @@ public class MusicPlayActivity extends AppCompatActivity {
         public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
             if (seekBar == sbVolume) {
                 DebugLog.debug("volume progress is " + progress);
+                int afterVolume = (int) (progress * 1.5);
+                if (fromUser){
+                    //手动拖动seekbar时才设置音量（排除外部改变音量的影响）
+                    mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, afterVolume, AudioManager.FLAG_PLAY_SOUND);
+                }
             } else if (seekBar == sbProgress) {
                 DebugLog.debug("music progress is " + progress);
             }
@@ -143,19 +180,22 @@ public class MusicPlayActivity extends AppCompatActivity {
         DebugLog.debug("mWindowManager--->" + mWindowManager);
         //设置window type
         wmParams.type = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
-
+        //设置背景为透明，否则滑动ListView会出现残影
+        wmParams.format = PixelFormat.TRANSPARENT;
         // FLAG_NOT_TOUCH_MODAL不阻塞事件传递到后面的窗口,不设置这个flag的话，home页的划屏会有问题
         int flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
         wmParams.flags = flags;
         //调整悬浮窗显示的停靠位置为右侧底部
-        wmParams.gravity = Gravity.RIGHT | Gravity.BOTTOM;
+        wmParams.gravity = Gravity.LEFT | Gravity.TOP;
         // 以屏幕右下角为原点，设置x、y初始值，相对于gravity
-        wmParams.x = 30;
-        wmParams.y = 400;
+        wmParams.x = 0;
+        wmParams.y = 0;
 
-        //  设置悬浮窗口长宽数据
-        wmParams.width = 600;
-        wmParams.height = 1100;
+//          设置悬浮窗口长宽数据
+//        wmParams.width = 600;
+//        wmParams.height = 1100;
+        wmParams.width = WindowManager.LayoutParams.MATCH_PARENT;
+        wmParams.height = WindowManager.LayoutParams.MATCH_PARENT;
 
         LayoutInflater inflater = LayoutInflater.from(getApplication());
         //获取浮动窗口视图所在布局
@@ -173,7 +213,116 @@ public class MusicPlayActivity extends AppCompatActivity {
             }
         });
 
+        mMusicListView = mFloatLayout.findViewById(R.id.lv_musicList);
+        mMusicListView.setAdapter(new MusicAdapter(mContext, musicInfo));
+
+        //监听返回键--隐藏悬浮窗口
+        mFloatLayout.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                switch (keyCode) {
+                    case KeyEvent.KEYCODE_BACK:
+                        mWindowManager.removeView(mFloatLayout);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+
+        /* 点击窗口外部区域可消除
+         将悬浮窗设置为全屏大小，外层有个透明背景，中间一部分视为内容区域,
+         所以点击内容区域外部视为点击悬浮窗外部
+         其中popupWindowView为全屏，listWindow为列表区域，触摸点没有落在列表区域，则隐藏列表*/
+
+        final View popupWindowView = mFloatLayout.findViewById(R.id.ll_popup_window);
+        final View listWindow = mFloatLayout.findViewById(R.id.ll_listWindow);
+        popupWindowView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                DebugLog.debug("touch");
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                Rect rect = new Rect();
+                listWindow.getGlobalVisibleRect(rect);
+                if (!rect.contains(x, y) && isShowList) {
+                    mWindowManager.removeView(mFloatLayout);
+                }
+                DebugLog.debug("onTouch : " + x + ", " + y + ", rect: " + rect);
+                return false;
+            }
+        });
 
     }
+
+    /**
+     * 注册音量广播接收器
+     * @return
+     */
+    public void registerReceiver() {
+        mMusicBroadcastReceiver = new MusicBroadcastReceiver();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(VOLUME_CHANGE_ACTION);
+        filter.addAction(VOLUME_MUTE);
+        mContext.registerReceiver(mMusicBroadcastReceiver, filter);
+        mRegistered = true;
+    }
+
+    /**
+     * 解注册音量广播监听器，需要与 registerReceiver 成对使用
+     */
+    public void unregisterReceiver() {
+        if (mRegistered) {
+            try {
+                mContext.unregisterReceiver(mMusicBroadcastReceiver);
+                mMusicBroadcastReceiver = null;
+                mRegistered = false;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver();
+    }
+
+    private class MusicBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            DebugLog.debug("receiver action " + intent.getAction());
+            //媒体音量改变才通知
+            if (VOLUME_CHANGE_ACTION.equals(intent.getAction())
+                    && (intent.getIntExtra(EXTRA_VOLUME_STREAM_TYPE, -1) == AudioManager.STREAM_MUSIC)) {
+                int changedVolume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+                DebugLog.debug("get change volume " + changedVolume);
+                Message msg = new Message();
+                msg.what = HANDLER_MESSAGE_REFRESH_VOLUME;
+                Bundle bundle = new Bundle();
+                bundle.putInt("volume",changedVolume);
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+            }
+        }
+    }
+
+
+    final Handler handler = new Handler(Looper.myLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            if (msg.what == HANDLER_MESSAGE_REFRESH_VOLUME) {
+                int volume = msg.getData().getInt("volume");
+                if (volume >= 0 && volume <= 150){
+                    int afterVolume = (int) (volume/1.5);
+                    sbVolume.setProgress(afterVolume);
+                }
+            }
+        }
+    };
+
 
 }
---
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/adapter/MusicAdapter.java
@@ -0,0 +1,72 @@
+package com.example.mediaplayproject.adapter;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.mediaplayproject.R;
+import com.example.mediaplayproject.bean.MediaFileBean;
+
+import java.util.List;
+
+/**
+ * @author wm
+ */
+public class MusicAdapter extends BaseAdapter {
+    private final Context mContext;
+    private List<MediaFileBean> musicInfoList;
+
+    public MusicAdapter(Context mContext, List<MediaFileBean> musicInfoList) {
+        this.mContext = mContext;
+        this.musicInfoList = musicInfoList;
+    }
+
+    public MusicAdapter(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    @Override
+    public int getCount() {
+        return musicInfoList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return musicInfoList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @SuppressLint("InflateParams")
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            //获取LayoutInflater实例
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.music_item,null);
+            holder = new ViewHolder();
+            holder.tvName = (TextView) convertView.findViewById(R.id.tv_music_name);
+            //将Holder存储到convertView中
+            convertView.setTag(holder);
+        } else {
+            //convertView不为空时，从convertView中取出Holder
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        holder.tvName.setText(musicInfoList.get(position).getTitle());
+        return convertView;
+    }
+
+
+
+    static class ViewHolder {
+        TextView tvName;
+    }
+}
