523d5ce150d1a99d2904ad1a48af1050c36a47b5
wmbilibili <2609228707@qq.com>
2023-09-10 22:02:11 +0800

    V1.8.1 解决点击通知栏无法进入app主页问题 [author]        wm [reason] 在SplashActivity中启动MainActivity时，虽然使用了NEW_TASK标志位，但和SplashActivity还是处于同一任务栈，且在创建之后清除了SplashActivity，所以点击通知栏返回MainActivity会出现异常； [solution] 取消在代码中添加启动MainActivity标志位，在AndroidManifest.xml中使用标志位-MainActivity以singleTask模式启动在"new.task"任务栈中，且使用excludeFromRecents属性隐藏SplashActivity的任务记录

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/activity/SplashActivity.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/activity/SplashActivity.java
@@ -47,8 +47,6 @@ public class SplashActivity extends BasicActivity {
                 public void onAnimationEnd(Animation animation) {
                     //动画结束时跳转到主页面
                     Intent intent =  new Intent(SplashActivity.this,MainActivity.class);
-                    // MainActivity以SingleTask方式启动，且进入MainActivity之后清除SplashActivity记录
-                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
                     startActivity(intent);
                 }
 
