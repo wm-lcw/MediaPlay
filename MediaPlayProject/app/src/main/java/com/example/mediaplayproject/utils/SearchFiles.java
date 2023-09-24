318105ee1d18e0051bdaf40c9649728d89284616
wmbilibili <2609228707@qq.com>
2023-09-24 19:29:28 +0800

    V2.5.6 解决app无法检索出flac格式音乐的问题 [author]        wm [reason] 使用MediaStore检索音乐时，限定了只检索MPEG 和WMA两种格式的音乐； [solution] 在过滤规则中增加flac格式的判断

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/utils/SearchFiles.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/utils/SearchFiles.java
@@ -38,7 +38,16 @@ public class SearchFiles {
             MediaStore.Audio.Media.DURATION,
             MediaStore.Audio.Media.SIZE
     };
-    private static final String SELECTION = "mime_type in ('audio/mpeg','audio/x-ms-wma') and bucket_display_name <> 'audio' and is_music > 0 ";
+
+    /** （1）"mime_type in ('audio/mpeg','audio/x-ms-wma')"：这部分条件指定了要查询的音频文件的 MIME 类型。
+        在这里，它指定了两种 MIME 类型：'audio/mpeg' 和 'audio/x-ms-wma'，分别代表 MPEG 格式的音频文件和 Windows Media Audio (WMA) 格式的音频文件。
+        表示查询结果将仅包含这两种格式的音频文件。可以加上'audio/flac'，可以检索到flac格式的音乐。
+     （2）"bucket_display_name <> 'audio'"：这部分条件用于排除存储桶（目录）名称为 'audio' 的文件。
+        通常，存储桶名称 'audio' 是用于存储系统音频文件的，例如系统通知音、铃声等。通过排除 'audio' 存储桶，你可以避免查询到这些系统音频文件。
+     （3）"is_music > 0"：这部分条件用于筛选出 "is_music" 列的值大于 0 的音频文件。
+        在 MediaStore.Audio.Media 表中， "is_music" 列通常用于标识音乐文件，其值大于 0 表示该文件是音乐文件。这个条件确保只有真正的音乐文件被包括在查询结果中。
+     */
+    private static final String SELECTION = "mime_type in ('audio/mpeg','audio/x-ms-wma','audio/flac') and bucket_display_name <> 'audio' and is_music > 0 ";
     private static final String SORT_ORDER = MediaStore.Audio.Media.DATA;
 
     public List<MediaFileBean> getMusicInfo() {
