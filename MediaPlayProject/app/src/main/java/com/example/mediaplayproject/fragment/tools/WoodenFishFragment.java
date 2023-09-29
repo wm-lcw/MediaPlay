6b048c68234d4279e7e0143f82a0baac0cdc5563
wmbilibili <2609228707@qq.com>
2023-09-29 12:03:19 +0800

    V2.8.6 增加电子木鱼Fragment中木鱼图片的点击事件 [author]        wm [description] 增加木鱼图片的点击效果；增加返回按钮的处理

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/fragment/tools/WoodenFishFragment.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/fragment/tools/WoodenFishFragment.java
@@ -8,9 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.example.mediaplayproject.R;
+import com.example.mediaplayproject.utils.ToolsUtils;
 
 
 /**
@@ -20,7 +23,8 @@ public class WoodenFishFragment extends Fragment {
 
     private View myView;
     private Context mContext;
-    private ImageView ivBack;
+    private ImageView ivBack,ivWoodenFish;
+    private Animation animation;
 
     public WoodenFishFragment() {
     }
@@ -53,6 +57,19 @@ public class WoodenFishFragment extends Fragment {
     }
 
     private void initView() {
+        ivBack = myView.findViewById(R.id.iv_wooden_fish_view_back);
+        ivWoodenFish = myView.findViewById(R.id.iv_wooden_fish);
+        ivBack.setOnClickListener(mListen);
+        ivWoodenFish.setOnClickListener(mListen);
+        animation = AnimationUtils.loadAnimation(mContext, R.anim.wooden_fish_icon_click_animation);
 
     }
+
+    private View.OnClickListener mListen = v -> {
+        if (v == ivBack) {
+            ToolsUtils.getInstance().backToMainViewFragment(mContext);
+        } else if (v == ivWoodenFish){
+            ivWoodenFish.startAnimation(animation);
+        }
+    };
 }
