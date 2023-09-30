5daa9e29818ff17854ce09de79f4e25c098a67ae
wmbilibili <2609228707@qq.com>
2023-09-30 20:00:21 +0800

    V2.8.7 优化电子木鱼的点击效果 [author]        wm [description] 增加点击弹出文字效果，并随机设定文字的颜色； 更换Fragment背景，替换木鱼图片

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/fragment/tools/WoodenFishFragment.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/fragment/tools/WoodenFishFragment.java
@@ -1,19 +1,27 @@
 package com.example.mediaplayproject.fragment.tools;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.example.mediaplayproject.R;
 import com.example.mediaplayproject.utils.ToolsUtils;
+
 
 
 /**
@@ -23,8 +31,11 @@ public class WoodenFishFragment extends Fragment {
 
     private View myView;
     private Context mContext;
-    private ImageView ivBack,ivWoodenFish;
+    private ImageView ivBack, ivWoodenFish;
     private Animation animation;
+    private RelativeLayout woodenFishRootView;
+    private TextView textView;
+    private static int[] colorList = {Color.WHITE,Color.YELLOW,Color.GREEN,Color.RED,Color.GRAY,Color.BLUE};
 
     public WoodenFishFragment() {
     }
@@ -37,7 +48,7 @@ public class WoodenFishFragment extends Fragment {
     }
 
     public static WoodenFishFragment getInstance(Context context) {
-        if (woodenFishFragment == null){
+        if (woodenFishFragment == null) {
             woodenFishFragment = new WoodenFishFragment(context);
         }
         return woodenFishFragment;
@@ -56,20 +67,48 @@ public class WoodenFishFragment extends Fragment {
         return myView;
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private void initView() {
+        woodenFishRootView = myView.findViewById(R.id.rl_wooden_fish_root_view);
         ivBack = myView.findViewById(R.id.iv_wooden_fish_view_back);
         ivWoodenFish = myView.findViewById(R.id.iv_wooden_fish);
         ivBack.setOnClickListener(mListen);
-        ivWoodenFish.setOnClickListener(mListen);
         animation = AnimationUtils.loadAnimation(mContext, R.anim.wooden_fish_icon_click_animation);
+        ivWoodenFish.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN){
+                    ivWoodenFish.startAnimation(animation);
+                    // 使用根布局addView添加一个TextView
+                    woodenFishRootView.addView(textView = new TextView(mContext));
+                    textView.setText("功德+1");
+                    textView.setTextSize(20);
+                    int color = (int)(Math.random()*colorList.length);
+                    textView.setTextColor(colorList[color]);
+                    //实现轨迹动画的代码
+                    AnimatorSet animSet = new AnimatorSet();
+                    // X轴的移动
+                    ObjectAnimator anim1 = ObjectAnimator.ofFloat(textView, "translationX", event.getRawX(), event.getRawX());
+                    // Y轴的移动
+                    ObjectAnimator anim2 = ObjectAnimator.ofFloat(textView, "translationY", event.getRawY(), 0f);
+                    //移动过程中逐渐透明
+                    ObjectAnimator anim3 = ObjectAnimator.ofFloat(textView, "alpha", 1f, 0f);
 
+                    AnimatorSet.Builder builder = animSet.play(anim2);
+                    builder.with(anim1).with(anim2).with(anim3);//三个动画一起执行
+                    animSet.setDuration(3500);//整个过程持续3.5s
+                    animSet.start(); // 开始播放属性动画
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     private View.OnClickListener mListen = v -> {
         if (v == ivBack) {
             ToolsUtils.getInstance().backToMainViewFragment(mContext);
-        } else if (v == ivWoodenFish){
-            ivWoodenFish.startAnimation(animation);
         }
     };
+
 }
