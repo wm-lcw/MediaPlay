ca4252d239a90c539d58c7e10c9a177c0bd7afca
wmbilibili <2609228707@qq.com>
2023-10-09 20:31:24 +0800

    V3.0.7 增加日志保存到文件的功能 [author]        wm [reason] 增加日志保存到文件的功能

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/utils/DebugLog.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/utils/DebugLog.java
@@ -1,11 +1,23 @@
 package com.example.mediaplayproject.utils;
 
+import android.annotation.SuppressLint;
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
  * @author wm
  */
 public class DebugLog {
     private static final boolean LOCAL_DBG_SWITCH = true;
     private static final String TAG = "MediaProjectLog";
+    private static final int LEVEL_FILE = 0x2;
 
     public static void info(String log) {
         if (LOCAL_DBG_SWITCH) {
@@ -15,6 +27,7 @@ public class DebugLog {
             String strMsg = currentStack.getFileName() + "(" + currentStack.getLineNumber() + ")::"
                     + currentStack.getMethodName() + " - " + log;
             android.util.Log.i(TAG, strMsg);
+            writerLog(LEVEL_FILE, strMsg);
         }
     }
 
@@ -26,6 +39,7 @@ public class DebugLog {
             String strMsg = currentStack.getFileName() + "(" + currentStack.getLineNumber() + ")::"
                     + currentStack.getMethodName() + " - " + log;
             android.util.Log.d(TAG, strMsg);
+            writerLog(LEVEL_FILE, strMsg);
         }
     }
 
@@ -37,6 +51,64 @@ public class DebugLog {
             String strMsg = currentStack.getFileName() + "(" + currentStack.getLineNumber() + ")::"
                     + currentStack.getMethodName() + " - " + log;
             android.util.Log.e(TAG, strMsg);
+            writerLog(LEVEL_FILE, strMsg);
         }
+    }
+
+    /**
+     * 路径 "/storage/emulated/0/mediaPlayLog"
+     * @param msg 需要打印的内容
+     */
+    public static void writerLog(int logLevel, String msg) {
+        if (LEVEL_FILE == logLevel) {
+            //保存到的文件路径
+            final String filePath = Environment.getExternalStorageDirectory().getAbsolutePath();
+            FileWriter fileWriter;
+            BufferedWriter bufferedWriter = null;
+
+            try {
+                //创建文件夹
+                File dir = new File(filePath, "mediaPlayLog");
+                if (!dir.exists()) {
+                    dir.mkdir();
+                }
+                //创建文件
+                File file = new File(dir, "mediaPlayLog.txt");
+                if (!file.exists()) {
+                    file.createNewFile();
+                }
+                //写入日志文件
+                fileWriter = new FileWriter(file, true);
+                bufferedWriter = new BufferedWriter(fileWriter);
+                bufferedWriter.write(  getCurrentTime() + "---" + msg  + "\n");
+                bufferedWriter.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (bufferedWriter != null) {
+                    try {
+                        bufferedWriter.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        } else {
+            Log.d("mediaPlayLog", msg+"");
+        }
+
+    }
+
+    /**
+     *  获取当前时间
+     *  @author wm
+     *  @createTime 2023/10/9 20:27
+     * @return : java.lang.String
+     */
+    private static String getCurrentTime() {
+        Calendar calendar = Calendar.getInstance();
+        @SuppressLint("SimpleDateFormat")
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return sdf.format(calendar.getTime());
     }
 }
