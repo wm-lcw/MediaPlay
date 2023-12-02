ca5adacfd72769b1cca18f330be10c5a05cc35dd
wmbilibili <2609228707@qq.com>
2023-12-02 17:28:59 +0800

    V3.2.2 设置页面-实现返回按钮逻辑、实现打印开关控制功能 [author]        wm [description] 设置页面-实现返回按钮逻辑、实现打印开关控制功能

--- MediaPlayProject/app/src/main/java/com/example/mediaplayproject/fragment/SettingsFragment.java
+++ MediaPlayProject/app/src/main/java/com/example/mediaplayproject/fragment/SettingsFragment.java
@@ -8,8 +8,16 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.mediaplayproject.R;
+import com.example.mediaplayproject.utils.Constant;
+import com.example.mediaplayproject.utils.DebugLog;
+import com.example.mediaplayproject.utils.SharedPreferencesUtil;
+import com.example.mediaplayproject.utils.ToolsUtils;
 
 
 /**
@@ -20,6 +28,8 @@ public class SettingsFragment extends Fragment {
     private static SettingsFragment mSettingsFragment;
     private Context mContext;
     private View mView;
+    private ImageView ivBack;
+    private Switch swLog, swLockScreen;
 
     public SettingsFragment(Context context) {
         mContext = context;
@@ -43,6 +53,40 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         mView = inflater.inflate(R.layout.fragment_settings, container, false);
+        initView();
+        initData();
         return mView;
     }
+
+    private void initView() {
+        ivBack = mView.findViewById(R.id.iv_settings_back);
+        swLog = mView.findViewById(R.id.sw_log);
+
+
+        ivBack.setOnClickListener(mListener);
+        swLog.setOnCheckedChangeListener(mCheckedChangeListener);
+    }
+
+    private void initData() {
+            //   打印控制逻辑
+            boolean isOpenLog = (Boolean) SharedPreferencesUtil.getData(Constant.LOG_SWITCH,true);
+            DebugLog.debug("isOpenLog " + isOpenLog);
+            swLog.setChecked(isOpenLog);
+    }
+
+    private final View.OnClickListener mListener = view -> {
+        if (view == ivBack) {
+            ToolsUtils.getInstance().backToMainViewFragment(mContext);
+        }
+    };
+
+    private final CompoundButton.OnCheckedChangeListener mCheckedChangeListener = (buttonView, isChecked) -> {
+        if (buttonView == swLog && buttonView.isPressed()){
+            boolean result = SharedPreferencesUtil.putData(Constant.LOG_SWITCH, isChecked);
+            DebugLog.debug("set log switch " + isChecked  + "; result: " + result);
+            if (result){
+                Toast.makeText(mContext,"设置成功，重启生效！", Toast.LENGTH_LONG).show();
+            }
+        }
+    };
 }
